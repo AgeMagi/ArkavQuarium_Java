2a3f76683c679ddd0fd9f63f9c167dfc2db62055
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-04-25 05:39:15 +0700

    New Junit

---
+++ src/GuppyTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GuppyTest {
+
+    @Test
+    public void gerak() {
+        Guppy s1 = new Guppy( 99, 640, 90 , 10000);
+        MakananIkan pointtujuan = new MakananIkan(1, 640);
+        while(pointtujuan.getX()<s1.getX()){
+            s1.gerak();
+        }
+        assertEquals(s1.getX(), pointtujuan.getX(), 0.1);
+    }
+
+    @Test
+    public void cariMakananIkanTerdekat() {
+        Ikan p = new Guppy(5,2,0,0);
+        MakananIkan g1 = new MakananIkan(10, 10);
+        MakananIkan g2 = new MakananIkan(0, 10);
+        MakananIkan g3 = new MakananIkan(10, 0);
+        MakananIkan g4 = new MakananIkan(0, 0);
+        List<MakananIkan> makanan = new List<MakananIkan>();
+        makanan.add(g1);
+        makanan.add(g2);
+        makanan.add(g3);
+        makanan.add(g4);
+        assertEquals(2, ((Guppy) p).cariMakananIkanTerdekat(makanan), 0.0001);
+    }
+
+    @Test
+    public void cariMakanGuppy() {
+        Ikan p = new Guppy(5,2,0,0);
+        MakananIkan g1 = new MakananIkan(10, 10);
+        MakananIkan g2 = new MakananIkan(0, 10);
+        MakananIkan g3 = new MakananIkan(10, 0);
+        MakananIkan g4 = new MakananIkan(0, 0);
+        List<MakananIkan> makanan = new List<MakananIkan>();
+        makanan.add(g1);
+        makanan.add(g2);
+        makanan.add(g3);
+        makanan.add(g4);
+        System.out.println(p.cariMakanGuppy(makanan));
+    }
+}
