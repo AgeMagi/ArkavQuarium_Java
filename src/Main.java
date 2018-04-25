f3ddcaac2ba41dc09b951a84817fac66b7768ffb
AgeMagi <muhhabibih@gmail.com>
2018-04-25 20:59:09 +0700

    FIX BANGET PROGRAM

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,29 @@ public class Main {
    */
   public static void main(String[] args) {
     try {
+      /**
+       * Untuk menginisiasi Akuarium yang akan digunakan untuk
+       * permainan ArkavQuarium dengan defaultImage pada parameter.
+       */
       Akuarium akuarium = new Akuarium("image/guppy1kiri.png");
+
+      /**
+       * Untuk mengatur ukuran dari akuarium dan juga posisi akuarium
+       * pada layar.
+       */
       akuarium.buildFrame();
 
+      /**
+       * Untuk mendeteksi input user seperti mouse click, keyboard clik.
+       */
       akuarium.setContent();
+
+      /**
+       * Untuk membuat GUI dari ArkavQuarium. Pada method startAkuarium
+       * terdapat method syncAll yang berguna untuk mengupdate posisi
+       * dari setiap objek pada akuarium. Terdapat juga method paintComponent
+       * yang berguna sebagai controller dari GUI tersebut.
+       */
       akuarium.startAkuarium();
     } catch (IOException e) {
       e.printStackTrace();
